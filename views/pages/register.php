1af7eb68c8307f41287e7ec93de84ac57aeb0f2c
CesarAntolinez <cesaralejandroantolinez@gmail.com>
2021-01-14 16:18:28 -0500

    mejorar apariencia register

--- views/pages/register.php
+++ views/pages/register.php
@@ -1,44 +1,41 @@
 <section class="container">
     <div class="row">
-        <div class="col">
-            <form class="row g-3">
-                <div class="col-md-6">
-                    <label for="inputEmail4" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4">
-                </div>
-                <div class="col-md-6">
-                    <label for="inputPassword4" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="inputPassword4">
+        <div class="d-flex justify-content-center text-center">
+            <form class="g-3 p-5 bg-light" method="post" action="index.php">
+                <div class="col-12">
+                    <label for="nik-name" class="form-label">Nik Name</label>
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text" style="display: block;"><i class="fas fa-user"></i></div>
+                        </div>
+                        <input type="email" class="form-control" name="nik-name" id="nik-name">
+                    </div>
                 </div>
                 <div class="col-12">
-                    <label for="inputAddress" class="form-label">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                    <label for="email" class="form-label">Email</label>
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text" style="display: block;"><i class="fas fa-envelope"></i></div>
+                        </div>
+                        <input type="email" class="form-control" name="email" id="email">
+                    </div>
                 </div>
                 <div class="col-12">
-                    <label for="inputAddress2" class="form-label">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                </div>
-                <div class="col-md-6">
-                    <label for="inputCity" class="form-label">City</label>
-                    <input type="text" class="form-control" id="inputCity">
-                </div>
-                <div class="col-md-4">
-                    <label for="inputState" class="form-label">State</label>
-                    <select id="inputState" class="form-select">
-                        <option selected>Choose...</option>
-                        <option>...</option>
-                    </select>
-                </div>
-                <div class="col-md-2">
-                    <label for="inputZip" class="form-label">Zip</label>
-                    <input type="text" class="form-control" id="inputZip">
+                    <label for="password" class="form-label">Password</label>
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text" style="display: block;"><i class="fas fa-key"></i></div>
+                        </div>
+                        <input type="password" class="form-control" name="password" id="password">
+                    </div>
                 </div>
                 <div class="col-12">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">
-                            Check me out
-                        </label>
+                    <label for="repeat-password" class="form-label">Repeat Password</label>
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text" style="display: block;"><i class="fas fa-key"></i></div>
+                        </div>
+                        <input type="password" class="form-control" name="repeat-password" id="repeat-password">
                     </div>
                 </div>
                 <div class="col-12">
